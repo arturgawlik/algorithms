d9e7cff41a822d79c0f1023ed8cc36e4c408836f
arturgawlik <artur.gawlik@icloud.com>
2025-03-16 13:13:41 +0100

    fix test suite name

--- algorithms/sorting/qucik-sort/test/quick-sort.test.ts
+++ algorithms/sorting/qucik-sort/test/quick-sort.test.ts
@@ -1,4 +1,4 @@
 import { generateSortTestForImpl } from "../../../../common-test-cases/selection-sort.ts";
 import { quickSort } from "../quick-sort.ts";
 
-generateSortTestForImpl("Selection Sort", quickSort);
+generateSortTestForImpl("Quick Sort", quickSort);
