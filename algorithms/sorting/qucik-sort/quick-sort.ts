596278352954d7484e7f83b40a4b38f77331aa07
arturgawlik <artur.gawlik@icloud.com>
2025-03-29 14:40:01 +0100

    quicksort improvements

--- algorithms/sorting/qucik-sort/quick-sort.ts
+++ algorithms/sorting/qucik-sort/quick-sort.ts
@@ -1,17 +1,51 @@
 export function quickSort<T>(array: T[], compareCb: (a: T, b: T) => boolean) {
-  // base case
+  // BASE CASE
   if (array.length < 2) {
     return array;
   }
 
-  // recursive case
-  const pivot = array[0];
-  const beforePivot = array.filter((i) => compareCb(i, pivot));
-  const afterThanPivot = array.filter((i) => compareCb(pivot, i));
+  // RECURSIVE CASE
+  // picking pivot from the middle is more optimal for cases when e.g. input array is already sorted
+  // const pivot = array[Math.round(array.length / 2)];
+  // picking pivot as random is most optimal in average for quick-sort
+  const pivot = array[random(0, array.length - 1)];
+  const { predicantPass: beforePivot, predicantFail: afterPivot } =
+    array.filterWithRest((i) => compareCb(i, pivot));
 
   return [
     ...quickSort(beforePivot, compareCb),
     pivot,
-    ...quickSort(afterThanPivot, compareCb),
+    ...quickSort(afterPivot, compareCb),
   ];
+}
+
+/**
+ * Enables iterating through array only one time and get new array that pass predicant and also not.
+ */
+Array.prototype.filterWithRest = function <T>(predicant: (item: T) => boolean) {
+  const predicantPass = [];
+  const predicantFail = [];
+  for (const item of this) {
+    if (predicant(item)) {
+      predicantPass.push(item);
+    } else {
+      predicantFail.push(item);
+    }
+  }
+
+  return { predicantPass, predicantFail };
+};
+
+function random(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+declare global {
+  interface Array<T> {
+    filterWithRest<T>(predicant: (item: T) => boolean): {
+      predicantPass: Array<T>;
+      predicantFail: Array<T>;
+    };
+  }
 }
